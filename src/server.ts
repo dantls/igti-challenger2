4f7afa0d12cfc9daf19545b8a43173ad59c0f81f
dantls <dan_tls@hotmail.com>
2020-08-08 00:55:18 -0400

    Setup finished

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-app.listen(3333, ()=>{
+app.listen(3333, () => {
   console.log('Server no ar!')
 })
